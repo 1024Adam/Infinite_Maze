8b7a3bd6e46375bae6fc34c66f58fbc13e357cb1
1024Adam <adamjreid10@gmail.com>
2017-02-11 21:09:47 -0500

    Added time and score

--- game.py
+++ game.py
@@ -18,31 +18,53 @@ pygame.display.set_icon(icon)
 bgColor = pygame.Color(255, 255, 255)
 lineColor = pygame.Color(0, 0, 0) 
 
-#Player/Position Details
+# Player/Position Details
 player = pygame.image.load('img/player.png')
 positionx = 5
 positiony = height / 2
 position = (positionx, positiony)
 
+# Game Config/Stats
 moveSpeed = 8
-
+font = pygame.font.SysFont('', 20)
+score = 0
+time = pygame.time.Clock()
+startTime = time.get_time()
+currMills = 0
+currSecs = 0
+currMins = 0
 gameOver = 0
 
 while (not gameOver):
+    # Paint Screen
     screen.fill(bgColor)
     screen.blit(player, position)
+    time.tick()
+    currMills += time.get_time()
+    currSecs = currMills / 1000
+    if (currSecs >= 60):
+        currMins += 1
+        currSecs -= 60
+    timeText = font.render('Time: ' + '{:02d}'.format(currMins) + ':' + '{:02d}'.format(currSecs), 1, Color(0, 0, 0))
+    screen.blit(timeText, (10, 10))
+    scoreText = font.render('Score: ' + str(score), 1, Color(0, 0, 0))
+    screen.blit(scoreText, (10, 25))
     pygame.display.flip()
   
+    # Arrow Move Events
     keys = pygame.key.get_pressed()
-    if (keys[pygame.K_RIGHT]):
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]):
         positionx += moveSpeed
-    elif (keys[pygame.K_DOWN]):
+        score += 1
+    elif (keys[pygame.K_DOWN] or keys[pygame.K_s]):
         positiony += moveSpeed
-    elif (keys[pygame.K_UP]):
+    elif (keys[pygame.K_UP] or keys[pygame.K_w]):
         positiony -= moveSpeed
-    elif (keys[pygame.K_LEFT]):
+    elif (keys[pygame.K_LEFT] or keys[pygame.K_a]):
         positionx -= moveSpeed
+        score -= 1
     
+    # Quit Events
     if (keys[pygame.K_LMETA] and keys[pygame.K_q]):
         gameOver = 1
     if (keys[pygame.K_RMETA] and keys[pygame.K_q]):
@@ -52,14 +74,18 @@ while (not gameOver):
     if (keys[pygame.K_ESCAPE]):
         gameOver = 1
     
+    # Process Game Events
     for event in pygame.event.get():
         if (event.type == pygame.QUIT):
             gameOver = 1
-    
+   
+    # Position Adjustments (to prevent screen overflow) 
     if (positionx < 5):
         positionx = 5
+        score += 1
     if (positionx > 620):
         positionx = 620
+        score -= 1
     if (positiony < 5):
         positiony = 5
     if (positiony > 460):
