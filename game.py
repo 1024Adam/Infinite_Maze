75a0a386c42e6b0165ec4453ba72d03a856cf6c1
1024Adam <adamjreid10@gmail.com>
2017-02-12 11:44:16 -0500

    Dont ask what I did to make it work... (wall collisions complete)

--- game.py
+++ game.py
@@ -6,6 +6,10 @@ pygame.init()
 # Screen Details
 width = 640
 height = 480
+maxWidthR = (width / 2)
+maxWidthL = 80
+maxHeightU = 40
+maxHeightD = (height - 40)
 screen = pygame.display.set_mode((width, height))
 icon = pygame.Surface((32, 32))
 iconImage = pygame.image.load('img/icon.png')
@@ -20,14 +24,12 @@ lineColor = pygame.Color(0, 0, 0)
 
 # Player/Position Details
 player = pygame.image.load('img/player.png')
-positionx = 5
+positionx = 80
 positiony = height / 2
 position = (positionx, positiony)
 
 # Game Config/Stats
-moveSpeed = 8
-xcollisionSpace = 3 * moveSpeed
-ycollisionSpace = 3 * moveSpeed
+moveSpeed = 10
 font = pygame.font.SysFont('', 20)
 score = 0
 time = pygame.time.Clock()
@@ -38,18 +40,20 @@ currMins = 0
 gameOver = 0
 
 # Maze Details
-lineCount = 5
+lineCount = 6
 lines = [[0 for x in range(2)] for y in range(20)]
-lines[0][0] = (100, 230)
-lines[0][1] = (105, 230)
-lines[1][0] = (105, 230)
-lines[1][1] = (110, 230)
-lines[2][0] = (110, 230)
-lines[2][1] = (115, 230)
-lines[3][0] = (115, 230)
-lines[3][1] = (120, 230)
-lines[4][0] = (100, 265)
-lines[4][1] = (105, 265)
+lines[0][0] = (120, 230)
+lines[0][1] = (125, 230)
+lines[1][0] = (125, 230)
+lines[1][1] = (130, 230)
+lines[2][0] = (130, 230)
+lines[2][1] = (135, 230)
+lines[3][0] = (135, 230)
+lines[3][1] = (140, 230)
+lines[4][0] = (190, 265)
+lines[4][1] = (195, 265)
+lines[5][0] = (195, 265)
+lines[5][1] = (200, 265)
 
 while (not gameOver):
     # Paint Screen
@@ -75,7 +79,7 @@ while (not gameOver):
     if (keys[pygame.K_RIGHT] or keys[pygame.K_d]):
         blocked = 0
         for row in range(lineCount):
-            if (((positionx + xcollisionSpace) > lines[row][0][0]) and ((positionx - xcollisionSpace) < lines[row][0][0]) and ((positiony - ycollisionSpace) < lines[row][0][1]) and ((positiony + ycollisionSpace) > lines[row][0][1])):
+            if ((positionx + 10 + moveSpeed >= lines[row][0][0]) and (positionx + 10 <= lines[row][0][0]) and (positiony + 20 > lines[row][0][1]) and (positiony - 5 < lines[row][0][1])):
                 blocked = 1
         if (not blocked):
             positionx += moveSpeed
@@ -83,15 +87,25 @@ while (not gameOver):
     if (keys[pygame.K_DOWN] or keys[pygame.K_s]):
         blocked = 0
         for row in range(lineCount):
-            if (((positionx + xcollisionSpace) > lines[row][0][0]) and ((positionx - xcollisionSpace) < lines[row][0][0]) and ((positiony - ycollisionSpace) < lines[row][0][1]) and ((positiony + ycollisionSpace) > lines[row][0][1])):
+            if ((positiony + 15 + moveSpeed >= lines[row][0][1]) and (positiony + 15 <= lines[row][0][1]) and (positionx + 15 > lines[row][0][0]) and (positionx - 15 < lines[row][0][0])):
                 blocked = 1
         if (not blocked):
             positiony += moveSpeed
     if (keys[pygame.K_UP] or keys[pygame.K_w]):
-        positiony -= moveSpeed
+        blocked = 0
+        for row in range(lineCount):
+            if ((positiony - moveSpeed <= lines[row][0][1]) and (positiony >= lines[row][0][1]) and (positionx + 15 > lines[row][0][0]) and (positionx - 15 < lines[row][0][0])):
+                blocked = 1
+        if (not blocked):
+            positiony -= moveSpeed
     if (keys[pygame.K_LEFT] or keys[pygame.K_a]):
-        positionx -= moveSpeed
-        score -= 1
+        blocked = 0
+        for row in range(lineCount):
+            if ((positionx - 5 - moveSpeed <= lines[row][0][0]) and (positionx - 5 >= lines[row][0][0]) and (positiony + 20 > lines[row][0][1]) and (positiony - 5 < lines[row][0][1])):
+                blocked = 1
+        if (not blocked):
+            positionx -= moveSpeed
+            score -= 1
     
     # Quit Events
     if (keys[pygame.K_LMETA] and keys[pygame.K_q]):
@@ -109,21 +123,21 @@ while (not gameOver):
             gameOver = 1
    
     # Position Adjustments (to prevent screen overflow) 
-    if (positionx < 5):
-        positionx = 5
-        for row in range(lineCount):
-            lines[row][0] = (lines[row][0][0] + moveSpeed, lines[row][0][1])
-            lines[row][1] = (lines[row][1][0] + moveSpeed, lines[row][1][1])
-        score += 1
-    if (positionx > 310):
-        positionx = 310
+    if (positionx < maxWidthL):
+        positionx = maxWidthL
+        score = max(score, 0)
+        if (score > 0):
+            for row in range(lineCount):
+                lines[row][0] = (lines[row][0][0] + moveSpeed, lines[row][0][1])
+                lines[row][1] = (lines[row][1][0] + moveSpeed, lines[row][1][1])
+    if (positionx > maxWidthR):
+        positionx = maxWidthR
         for row in range(lineCount):
             lines[row][0] = (lines[row][0][0] - moveSpeed, lines[row][0][1])
             lines[row][1] = (lines[row][1][0] - moveSpeed, lines[row][1][1])
-    if (positiony < 5):
-        positiony = 5
-    if (positiony > 460):
-        positiony = 460    
+    positiony = max(positiony, maxHeightU)
+    positiony = min(positiony, maxHeightD)
+    
     position = (positionx, positiony)
 
 pygame.quit() 
