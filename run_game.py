e7c3082687e7bc4b0467f26f9af4c0fc0c7ec987
Adam Reid <areid@brocksolutions.com>
2025-07-02 23:03:32 -0400

    run game improvements

--- run_game.py
+++ run_game.py
@@ -6,10 +6,15 @@ This script allows running the game with: python run_game.py
 
 import sys
 import os
+import warnings
+
+# Suppress pygame's pkg_resources deprecation warning
+warnings.filterwarnings("ignore", message="pkg_resources is deprecated as an API")
+warnings.filterwarnings("ignore", category=UserWarning, module="pygame.pkgdata")
 
 # Add current directory to Python path
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == '__main__':
-    from infinite_maze.infinite_maze import maze
+    from infinite_maze.core.engine import maze
     maze()
